7d1ae951f150cd077cd22120c58fd3224042cb04
carsonjf8 <carsonjf8@gmail.com>
2022-04-13 22:54:46 -0400

    DA3B set server address

--- DA3B/frontend/index.js
+++ DA3B/frontend/index.js
@@ -1,7 +1,7 @@
 // local development socket
 // const socket = io('localhost:3000');
 // heroku deployment socket
- const socket = io('https://powerful-lake-99065.herokuapp.com/');
+ const socket = io('https://thawing-cliffs-15940.herokuapp.com/');
 
 // initial screen components
 const initialScreen = document.getElementById('initialScreen');
