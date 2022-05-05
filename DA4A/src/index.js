3f68e86ff5d0d60780bd8e570b5079fd193a171e
carsonjf8 <carsonjf8@gmail.com>
2022-05-05 13:23:46 -0400

    added DB4B link

--- DA4A/src/index.js
+++ DA4A/src/index.js
@@ -35,9 +35,9 @@ scene.add(light);
 const controls = new FlyControls(camera, renderer.domElement);
 controls.autoForward = true;
 controls.dragToLook = false;
-controls.enablePan = false;
-controls.movementSpeed = 1;
+controls.movementSpeed = 3;
 controls.rollSpeed = 1;
+controls.domElement.removeEventListener( 'mousemove', controls.mousemove );
 
 let envmap;
 let tick = 0;
@@ -62,10 +62,43 @@ let tick = 0;
             envMapIntensity: 0.1,
         })
     );
-    groundMesh.geometry.translate(10, 0, 10);
     groundMesh.castShadow = true;
     groundMesh.receiveShadow = true;
     scene.add(groundMesh);
+
+    let buildingGeometry = new BoxGeometry(0, 0, 0);
+
+    let bg1 = new BoxGeometry(2, 6, 2);
+    bg1.translate(0, 3, 0);
+    buildingGeometry = mergeBufferGeometries([buildingGeometry, bg1]);
+
+    let bg2 = new BoxGeometry(2, 6, 2);
+    bg2.translate(4.5, 3, 0);
+    buildingGeometry = mergeBufferGeometries([buildingGeometry, bg2]);
+
+    let bg3 = new BoxGeometry(2, 6, 2);
+    bg3.translate(9, 3, 0);
+    buildingGeometry = mergeBufferGeometries([buildingGeometry, bg3]);
+
+    let bg4 = new BoxGeometry(7, 1, 1);
+    bg4.translate(4.5, 2.5, 0);
+    buildingGeometry = mergeBufferGeometries([buildingGeometry, bg4]);
+
+    let bg5 = new BoxGeometry(2.5, 1, 1);
+    bg5.translate(2.25, 5.5, 0);
+    buildingGeometry = mergeBufferGeometries([buildingGeometry, bg5]);
+
+    let buildingMesh = new Mesh(
+        buildingGeometry,
+        new MeshPhysicalMaterial({
+            envMap: envmap,
+            map: null,
+            envMapIntensity: 0.1,
+        })
+    );
+    buildingMesh.castShadow = true;
+    buildingMesh.receiveShadow = true;
+    scene.add(buildingMesh);
 
     renderer.setAnimationLoop(() => {
         const delta = clock.getDelta();
